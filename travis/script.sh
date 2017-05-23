1b178ff0197ec97b0a5563db4d6b113377b5b830
Konrad Grochowski <kgrochowski@n7mobile.com>
2017-05-23 13:58:34 +0200

    ASNIDE-33 Travis will build binaries with and without tests

--- travis/script.sh
+++ travis/script.sh
@@ -3,17 +3,19 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "${DIR}/setup-env.sh"
 
 if [[ $CI == "true" && $BUILD_OS_NAME == 'linux' ]]; then
-  . /opt/qt58/bin/qt58-env.sh
+    . /opt/qt58/bin/qt58-env.sh
+    QMAKE_PARAMS="-spec linux-g++"
 fi
 
 echo "Building..."
 cd "${BUILD_DIR}"
 echo "Executing qmake..."
-qmake "${PROJECT_DIR}"/asn1acn.pro -r ${ENV_QMAKE_PARAMS} \
+set -x
+qmake "${PROJECT_DIR}"/asn1acn.pro -r ${QMAKE_PARAMS} \
       CONFIG+=release \
-      QTC_SOURCE="${DOWNLOAD_DIR}/qt-creator-opensource-src-${ENV_QTC_VERSION}"\
-      QTC_BUILD="${DOWNLOAD_DIR}/qtcbuild/${ENV_QTC_VERSION}"\
-      OUTPUT_PATH="${PLUGIN_OUT_DIR}"\
-      SET_VERSION_MINOR="${ENV_QTC_MV}"
+      BUILD_TESTS="${ENV_WITH_TESTS}" \
+      QTC_SOURCE="${DOWNLOAD_DIR}/qt-creator-opensource-src-${ENV_QTC_VERSION}" \
+      QTC_BUILD="${DOWNLOAD_DIR}/qtcbuild/${ENV_QTC_VERSION}"
+set +x
 echo "Executing make..."
 make
