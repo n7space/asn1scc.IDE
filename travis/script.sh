f08f3acc23a743afc1f37f488bb7661f4494e93b
Konrad Grochowski <kgrochowski@n7mobile.com>
2017-06-20 17:20:17 +0200

    fixed failing travis script when compilation fails

--- travis/script.sh
+++ travis/script.sh
@@ -15,10 +15,10 @@ qmake "${PROJECT_DIR}"/asn1acn.pro -r ${QMAKE_PARAMS} \
       CONFIG+=release \
       BUILD_TESTS="${ENV_WITH_TESTS}" \
       QTC_SOURCE="${DOWNLOAD_DIR}/qt-creator-opensource-src-${ENV_QTC_VERSION}" \
-      QTC_BUILD="${QTC_BUILD_DIR}"
+      QTC_BUILD="${QTC_BUILD_DIR}" || exit 1
 set +x
 echo "Executing make..."
-make
+make || exit 1
 
 if [[ $ENV_WITH_TESTS == "1" ]]; then
     echo "Starting xvfb..."
@@ -26,5 +26,5 @@ if [[ $ENV_WITH_TESTS == "1" ]]; then
     sh -e /etc/init.d/xvfb start
     sleep 3
     echo "Executing tests..."
-    ${QTC_BUILD_DIR}/bin/qtcreator -test "ASN.1/ACN"
+    ${QTC_BUILD_DIR}/bin/qtcreator -test "ASN.1/ACN" || exit 1
 fi
