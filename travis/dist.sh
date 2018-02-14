c071c3322e20deff4abe4d869e9567158a8bf4bb
Konrad Grochowski <kgrochowski@n7space.com>
2018-02-14 14:30:32 +0100

    distros will include "static files"

--- travis/dist.sh
+++ travis/dist.sh
@@ -6,12 +6,19 @@ if [[ $BUILD_OS_NAME == 'linux' ]]; then
     mkdir -p ${DIST_DIR}/lib/qtcreator/plugins
     cp ${QTC_BUILD_DIR}/lib/qtcreator/plugins/lib${PLUGIN_NAME}.so ${DIST_DIR}/lib/qtcreator/plugins
     strip ${DIST_DIR}/lib/qtcreator/plugins/lib${PLUGIN_NAME}.so
+    RESOURCE_DIST_DIR=${DIST_DIR}/share/qtcreator
     OS_TAR_SUFFIX="linux-x64"
 elif [[ $BUILD_OS_NAME == 'osx' ]]; then
     mkdir -p ${DIST_DIR}/Contents/PlugIns
+    RESOURCE_DIST_DIR=${DIST_DIR}/Contents/Resources
     cp "${QTC_BUILD_DIR}/Qt Creator.app/Contents/PlugIns/lib${PLUGIN_NAME}.dylib" ${DIST_DIR}/Contents/PlugIns
     OS_TAR_SUFFIX="osx"
 fi
+
+mkdir -p ${RESOURCE_DIST_DIR}
+cp -r ${PROJECT_DIR}/generic-highlighter ${RESOURCE_DIST_DIR}
+cp -r ${PROJECT_DIR}/templates ${RESOURCE_DIST_DIR}
+cp -r ${PROJECT_DIR}/snippets ${RESOURCE_DIST_DIR}
 
 pushd ${DIST_DIR}
 tar czf ${PROJECT_DIR}/${PLUGIN_NAME}-${VERSION}-${OS_TAR_SUFFIX}.tar.gz *
