b534d34396a7f719cf60e01e78c43be45cc78b14
Konrad Grochowski <kgrochowski@n7space.com>
2018-02-20 16:59:30 +0100

    Travis will use tar.gz asn1scc distro for better permissions

--- travis/dist.sh
+++ travis/dist.sh
@@ -21,6 +21,7 @@ fi
 
 echo "Downloading asn1scc to distribute with plugin"
 wget -O ${DOWNLOAD_DIR}/asn1scc.7z "https://drive.google.com/uc?export=download&id=1tzZdsXU4CHbppf4TQMrkNx-6p0zsCuEt"
+mkdir -p ${LIBEXEC_DIST_DIR}
 7zr x -o${LIBEXEC_DIST_DIR} ${DOWNLOAD_DIR}/asn1scc.7z
 
 echo "Copy static files"
