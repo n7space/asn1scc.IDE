60fad18e1ede62a1014854d8826477dd644ac2fa
Konrad Grochowski <kgrochowski@n7space.com>
2018-02-20 12:42:46 +0100

    no more stripping debug symbols on Travis

--- travis/dist.sh
+++ travis/dist.sh
@@ -8,15 +8,14 @@ echo "Copy lib"
 if [[ $BUILD_OS_NAME == 'linux' ]]; then
     mkdir -p ${DIST_DIR}/lib/qtcreator/plugins
     cp ${QTC_BUILD_DIR}/lib/qtcreator/plugins/lib${PLUGIN_NAME}.so ${DIST_DIR}/lib/qtcreator/plugins
-    strip ${DIST_DIR}/lib/qtcreator/plugins/lib${PLUGIN_NAME}.so
     RESOURCE_DIST_DIR=${DIST_DIR}/share/qtcreator
     LIBEXEC_DIST_DIR=${DIST_DIR}/libexec/qtcreator
     OS_TAR_SUFFIX="linux-x64"
 elif [[ $BUILD_OS_NAME == 'osx' ]]; then
     mkdir -p ${DIST_DIR}/Contents/PlugIns
+    cp "${QTC_BUILD_DIR}/Qt Creator.app/Contents/PlugIns/lib${PLUGIN_NAME}.dylib" ${DIST_DIR}/Contents/PlugIns
     RESOURCE_DIST_DIR=${DIST_DIR}/Contents/Resources
     LIBEXEC_DIST_DIR=${RESOURCE_DIST_DIR}
-    cp "${QTC_BUILD_DIR}/Qt Creator.app/Contents/PlugIns/lib${PLUGIN_NAME}.dylib" ${DIST_DIR}/Contents/PlugIns
     OS_TAR_SUFFIX="osx"
 fi
 
