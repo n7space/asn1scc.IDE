da5e254dbad3aebb3282d7064c581c0473523e99
Igor Jarek <ijarek@n7space.com>
2022-02-21 22:38:33 +0100

    update years (2)

--- src/usagesfinder.cpp
+++ src/usagesfinder.cpp
@@ -1,6 +1,6 @@
 /****************************************************************************
 **
-** Copyright (C) 2022 N7 Space sp. z o. o.
+** Copyright (C) 2017-2022 N7 Space sp. z o. o.
 ** Contact: https://n7space.com
 **
 ** This file is part of ASN.1/ACN Plugin for QtCreator.
